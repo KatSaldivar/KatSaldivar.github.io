382e4940649a95180234bdda0f8774daba3ee957
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-21 08:48:55 -0700

    more navbar work

---
+++ javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+      
+//do something about the active section to make the title stay at the top
+});
