89cbff17221aa9889c20cca8b3add12924e58702
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-21 12:01:33 -0700

    made navbar responsive

---
+++ portfolio-javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $("#navButton").click(function(){
+        if($("#navButton").text()=="☰"){
+            $("#navButton").text("✖");
+        }else{
+            $("#navButton").text("☰");
+        }
+        $("li").toggle("slow");
+    });
+});   
+//do something about the active section to make the title stay at the top
+//fix background color on hover
+//<i class="fa fa-close"></i>*/
+//https://itnext.io/how-to-create-a-responsive-navigation-bar-with-html-css-and-jquery-step-by-step-tutorial-9c780b58479f
