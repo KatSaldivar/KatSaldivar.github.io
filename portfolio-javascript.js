698dc08f9405d18dd72ce339fb3c3e65da68d1f9
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-21 12:23:47 -0700

    made nav collapse by default

--- portfolio-javascript.js
+++ portfolio-javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("#navButton").click(function(){
+    $("nav").click(function(){
         if($("#navButton").text()=="☰"){
             $("#navButton").text("✖");
         }else{
@@ -9,8 +9,5 @@ $(document).ready(function(){
         $("li").toggle("slow");
     });
 });   
-//do something about the active section to make the title stay at the top
 //fix background color on hover
-//hide nav when small by default
-//<i class="fa fa-close"></i>*/
 //https://itnext.io/how-to-create-a-responsive-navigation-bar-with-html-css-and-jquery-step-by-step-tutorial-9c780b58479f
