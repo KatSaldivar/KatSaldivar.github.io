cde51b6627b7913cd9226ac7dcac846b0deef834
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-21 12:13:15 -0700

    changed fonts form px to vh

--- portfolio-javascript.js
+++ portfolio-javascript.js
@@ -11,5 +11,6 @@ $(document).ready(function(){
 });   
 //do something about the active section to make the title stay at the top
 //fix background color on hover
+//hide nav when small by default
 //<i class="fa fa-close"></i>*/
 //https://itnext.io/how-to-create-a-responsive-navigation-bar-with-html-css-and-jquery-step-by-step-tutorial-9c780b58479f
