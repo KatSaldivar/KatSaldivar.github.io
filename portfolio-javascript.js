358c5891261f7728c5737efec2cae98a59160976
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-21 21:07:19 -0700

    more navbar improvements and bug fixes

--- portfolio-javascript.js
+++ portfolio-javascript.js
@@ -1,13 +1,47 @@
 $(document).ready(function(){
 
-    $("nav").click(function(){
-        if($("#navButton").text()=="☰"){
-            $("#navButton").text("✖");
-        }else{
-            $("#navButton").text("☰");
-        }
-        $("li").toggle("slow");
-    });
+    // Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// Navigation Scroll 
+$('a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
 });   
-//fix background color on hover
+//Resources: 
+//https://codepen.io/MightyShaban/pen/CIfdj
+
+
+//add shiny animation to button
+//make sure content is responsive
+//try scrolly image thing
 //https://itnext.io/how-to-create-a-responsive-navigation-bar-with-html-css-and-jquery-step-by-step-tutorial-9c780b58479f
