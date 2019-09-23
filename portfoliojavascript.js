88e9b53abcca95853c80fb6cc6ed2060e6c27a62
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-22 21:14:45 -0700

    fixed extra space when scrolling

--- portfoliojavascript.js
+++ portfoliojavascript.js
@@ -19,7 +19,7 @@ $('.main_h li a').click(function() {
 // Navigation Scroll 
 $('a').click(function(event) {
     var id = $(this).attr("href");
-    var offset = 70;
+    var offset = 50;
     var target = $(id).offset().top - offset;
     $('html, body').animate({
         scrollTop: target
