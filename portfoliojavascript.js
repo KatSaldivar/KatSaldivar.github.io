ff4f29a17928d5e6c1756c5b1dbef92b626d81dc
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-09-22 19:51:38 -0700

    portfolio section responsive

--- portfoliojavascript.js
+++ portfoliojavascript.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-    // Sticky Header
-/*$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});*/
-
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
     if ($('.main_h').hasClass('open-nav')) {
